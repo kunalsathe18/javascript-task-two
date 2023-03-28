572fe244a0a82c2d5416cf446b1ea729991580dc
kunalsathe18 <kunalsathe18@gmail.com>
2023-03-28 20:29:28 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+var v=10;
+var k=8
+var vk=10+8;
+console.log(vk);
+// console.log("operator-addition-over-here");
+
+var v=30;
+var k=12;
+var vk=30-12;
+console.log(vk);
+// console.log("operator-substraction-over-here");
+
+var v=9;
+var k=2;
+var vk=9*2;
+console.log(vk);
+// console.log("operator-multiplication-over-here");
+
+var v=36;
+var k=2;
+var vk=v/k;
+console.log(vk);
+// console.log("operator-division-over-here");
+
+
+var v=18;
+var k=18;
+var vk=v**k;
+console.log(vk);
+// console.log("operator-Exponentiation-over-here");
+
+// console.log("operator-increment-over-here");
+// console.log("operator-decrement-over-here");
+
+
+var v=18;
+var k=50;
+var vk=v%k;
+console.log(vk);
+// console.log("operator-reminder-over-here");
